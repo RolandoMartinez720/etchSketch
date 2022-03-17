7f9631dca2478c5ae75091df09dc2c8689ffeae0
Rolando <rolando.a.martinez@outlook.com>
2022-03-16 20:17:40 -0500

    figured out how to align grid correctly and center on page now i just need to add event listeners to the grid items to change colors and add a button to undo any changes

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let element;
     for(let i=0;i<400;i++){
     element=document.createElement('div');//create div and appendchild it 400 times
     element.classList.add('grid-item');
-    element.textContent="a";
     gridContainer.appendChild(element);
 }
 
