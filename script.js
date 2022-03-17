08cc00608f9cbe107dfd00d1dd26e1cb0fa74ab6
Rolando <rolando.a.martinez@outlook.com>
2022-03-16 19:53:21 -0500

    created all the divs but now i need to find a way to get them to fall into place correctly

---
+++ script.js
@@ -0,0 +1,10 @@
+let gridContainer=document.getElementById('grid-container');
+
+let element;
+    for(let i=0;i<400;i++){
+    element=document.createElement('div');//create div and appendchild it 400 times
+    element.classList.add('grid-item');
+    element.textContent="a";
+    gridContainer.appendChild(element);
+}
+
