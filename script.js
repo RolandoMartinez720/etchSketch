68b5de0586253dd8274def9958c36783cc1fbc86
Rolando <rolando.a.martinez@outlook.com>
2022-03-19 11:48:31 -0500

    redid the entire thing to meet the requirements of project

--- script.js
+++ script.js
@@ -1,24 +1,87 @@
-let gridContainer=document.getElementById('grid-container');
+//create a number of elements automatically
+//give them a class name and also an event listener
 
-let element;
-    for(let i=0;i<400;i++){
-    element=document.createElement('div');//create div and appendchild it 400 times
-    element.classList.add('grid-item');
-    gridContainer.appendChild(element);
+let container=document.getElementById('container');
+let textBox=document.getElementById('grid-size-input-box');
+let clears=document.getElementById('clearBtn');
+let gridSubmitBtn=document.getElementById('grid-submit');
+let colorPicker=document.getElementById('colorPicker');
+let rainbow=document.getElementById('rainbow');//SELECT BOX RAINBOW OPTION ID
+let color='black';
+let size=16;
+textBox.placeholder=size;
+
+populateBoard(size);
+createElements(size);
+
+//EVENT LISTENERS
+colorPicker.addEventListener('change', changeColor);//SETS COLOR
+gridSubmitBtn.addEventListener('click',changeSize)//CHANGES GRID SIZE
+clears.onclick=function(){clearBoard()}; //CLEARS BOARD
+
+//FUNCTION
+//============ Changes size of grid
+function changeSize(){
+    if(typeof Number(textBox.value)==='number'&& textBox.value<=100){
+    size=textBox.value;
+    clearBoard();
+    createElements(size);
+    populateBoard(size);}
+    else alert('invalid input: only numbers less than 100')
+}
+//FUNCTION
+//creates the elements based on input squared
+//creates event mouseover listener for each element
+//gives each element bgColor of default grey clr
+//adds element to container element
+function createElements(size){
+    for(let i=0;i<(size**2);i++){
+        gridItem=document.createElement('div');
+        gridItem.className='grid-item';
+        gridItem.style.backgroundColor='grey';
+        gridItem.addEventListener('mouseover',coloring);
+        container.appendChild(gridItem);
+    }
+}
+ 
+//FUNCTION
+//---------CREATES BOARD OF SIZE ${SIZE}
+function populateBoard(size){
+container.style.gridTemplateColumns= `repeat(${size} ,1fr)`;
+container.style.gridTemplateRows= `repeat(${size} ,1fr)`;
 }
 
-let gridElements=document.getElementsByClassName('grid-item');
-console.log(gridElements.length);
+//FUNCTION
+//--CHANGES VALUE OF EVENTLISTENER COLOR FOR ELEMENT TARGET CALLING FUNCTION
+//--TO THE CURRENT VALUE OF VAR COLOR
+function coloring(e){
+if(e.altKey)
+{setRainbow();//sets the value of rainbow
+changeColor();//changes the color to be the drop down box value
+e.target.style.backgroundColor=color;}
+}
 
-
-for(let i=0;i<gridElements.length;i++)
+//FUNCTION
+//---------- CLEAR BOARD FUNCTION
+function clearBoard()
 {
-    gridElements[i].addEventListener('mouseover',changeColor)
-    
+   let itemArr= document.getElementsByClassName('grid-item');
+    for(element of itemArr){
+        element.style.backgroundColor='grey';
+    }
+}
+//FUNCTION
+//SETS VALUE OF VARIABLE 'COLOR'
+function changeColor(){
+color=colorPicker.value;
 }
 
+//FUNCTION
+//---SETS RANDOM COLOR
 
-
-function changeColor(e){
-    e.target.style.backgroundColor='black';
+function setRainbow(){
+let r=Math.floor(Math.random()*255);
+let g=Math.floor(Math.random()*255);
+let b=Math.floor(Math.random()*255);
+rainbow.value=`rgb(${r},${g},${b})`;
 }
